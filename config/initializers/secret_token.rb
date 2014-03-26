50fd0fb3adfeae4fa1fb7dc7734945bca58c2cc5
Rabbit <aarkadiusz.dabrowski@gmail.com>
2014-03-26 22:25:42 +0100

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GistyDabrowski::Application.config.secret_key_base = '8b10daae430e2d52bf7f2dc9e9ce69f5c211bb4ff1661ac437aa9e45df0bba4f85448684826360ff4d54c2adeb6db8a363e4ee034ce16ad352b565ce31299668'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GistyDabrowski::Application.config.session_store :cookie_store, key: '_gisty_dabrowski_session'
